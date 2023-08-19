aa3aa98d5871902aaf3d52fea3b5441d59e7cfb2
ubuntu <ubuntu@ubuntu>
2023-08-19 11:28:01 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Demo
+
+Demo
+
+#### License
+
+MIT
---
+++ demo/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Demo",
+			"type": "module",
+			"label": _("Demo")
+		}
+	]
